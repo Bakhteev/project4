d6fc9837e2998ec0f8bd26b1d53cf8992799eb68
Bogdan-create <bogdan-shigaev@mail.ru>
2020-10-10 21:13:23 +0600

    finished portfolio, question

---
+++ js/index.js
@@ -0,0 +1,7 @@
+let dropDownTitle = document.getElementsByClassName('question__item__title')
+let dropDownItem = document.getElementsByClassName('question__item__description')
+
+
+element.onclick = function dropDown (dropDownItem) {
+    dropDownItem.style.display = 'block'
+}
