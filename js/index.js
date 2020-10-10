29e13a4318796f2a73da5f5ad29ec9e7b4115e9d
Bogdan-create <bogdan-shigaev@mail.ru>
2020-10-10 21:46:20 +0600

    fail X_X

--- js/index.js
+++
@@ -1,7 +0,0 @@
-let dropDownTitle = document.getElementsByClassName('question__item__title')
-let dropDownItem = document.getElementsByClassName('question__item__description')
-
-
-element.onclick = function dropDown (dropDownItem) {
-    dropDownItem.style.display = 'block'
-}
